51e7ed1d6e42235df5be5295a7fdd08b8874cbe0
Nick Nance <nick.nance@creditkarma.com>
2018-04-30 07:05:11 -0700

    generate structures

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs'
 import * as json2md from 'json2md'
-import thriftParser = require('thrift-parser')
+
+import {
+    parse,
+    StructDefinition,
+    ThriftDocument,
+} from '@creditkarma/thrift-parser'
 
 const loadFile = (fileName: string): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -14,17 +19,41 @@ const loadFile = (fileName: string): Promise<string> => {
     })
 }
 
+const transformStructs = (ast: ThriftDocument): any[] => {
+
+    const structures = ast.body
+    .filter((def) => def.type === 'StructDefinition')
+    .map((def: StructDefinition) => [{
+            h2: def.name.value,
+        }, {
+            table: {
+                headers: ['Key', 'Field', 'Type', 'Description', 'Required', 'Default value'],
+                rows: def.fields.map((fld) => [
+                    fld.fieldID ? fld.fieldID.value : '',
+                    fld.name.value,
+                    fld.fieldType.type,
+                    fld.comments.length ? fld.comments[0].value : '',
+                    fld.requiredness,
+                    fld.defaultValue || '',
+                ]),
+            },
+        }],
+    )
+
+    return [{ h1: 'Data Structures'}, structures]
+}
+
 const main = async () => {
     const data = await loadFile('./fixtures/thrift/metadata.thrift')
-    const json = thriftParser(data)
-    json2md.converters.namespace = (input) => `## ${Object.keys(input)[0]}\r\n` + json2md(input)
-    json2md.converters.java = (input) => `* ${input[0]}\r\n` + json2md(input)
-    json2md.converters.struct = (input) => `### ${input}\r\n` + json2md(input)
-    json2md.converters.serviceName = (input) => `### Service Name\r\n` + json2md(input)
-    const md = json2md(json)
-    console.dir(json)
-    console.dir(json.struct)
-    console.log(md)
+    const ast = parse(data)
+    if (ast.type === 'ThriftErrors') {
+        console.dir(ast, {depth: null})
+    } else {
+        const transform = transformStructs(ast as ThriftDocument)
+        const md = json2md(transform)
+        console.dir(ast, {depth: null})
+        console.log(md)
+    }
 }
 
 main()
