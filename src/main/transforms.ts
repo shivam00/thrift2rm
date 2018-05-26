1e939d0f152f6a3fa14d145bc9cad2ab0fde5a29
Nick Nance <nance.nick@gmail.com>
2018-05-26 14:42:35 -0700

    const transform

--- src/main/transforms.ts
+++ src/main/transforms.ts
@@ -2,15 +2,26 @@ import * as path from 'path'
 
 import {
     BaseType,
+    BooleanLiteral,
+    ConstList,
+    ConstMap,
+    ConstValue,
+    DoubleConstant,
+    ExponentialLiteral,
     FieldDefinition,
+    FloatLiteral,
     FunctionDefinition,
+    HexLiteral,
     Identifier,
+    IntConstant,
+    IntegerLiteral,
     ListType,
     MapType,
     NamespaceDefinition,
     PrimarySyntax,
     ServiceDefinition,
     SetType,
+    StringLiteral,
     StructDefinition,
     SyntaxNode,
     SyntaxType,
@@ -62,6 +73,66 @@ const transformField = (fld: SyntaxNode) =>
         (z) => `[${z.value}](#${z.value})`,
     )
 
+function literalTransform<U>(
+    r: StringLiteral | BooleanLiteral | IntegerLiteral | HexLiteral | FloatLiteral | ExponentialLiteral,
+    e: (_: StringLiteral) => U,
+    f: (_: BooleanLiteral) => U,
+    g: (_: IntegerLiteral) => U,
+    h: (_: HexLiteral) => U,
+    i: (_: FloatLiteral) => U,
+    j: (_: ExponentialLiteral) => U,
+): U {
+    switch (r.type) {
+        case SyntaxType.StringLiteral: return e(r as StringLiteral)
+        case SyntaxType.BooleanLiteral: return f(r as BooleanLiteral)
+        case SyntaxType.IntegerLiteral: return g(r as IntegerLiteral)
+        case SyntaxType.HexLiteral: return h(r as HexLiteral)
+        case SyntaxType.FloatLiteral: return i(r as FloatLiteral)
+        case SyntaxType.ExponentialLiteral: return j(r as ExponentialLiteral)
+        default: return e(r)
+    }
+}
+
+declare type LiteralValue = StringLiteral | BooleanLiteral | IntegerLiteral | HexLiteral |
+FloatLiteral | ExponentialLiteral
+
+const getLiteralVal = (
+    fld: LiteralValue,
+) => literalTransform(
+    fld,
+    (e) => `"${e.value}"`,
+    (f) => `${f.value}`,
+    (g) => `${g.value}`,
+    (h) => `#${h.value}`,
+    (i) => `${i.value}`,
+    (j) => `${j.value}`,
+)
+
+function constTransform<U>(
+    r: ConstValue,
+    e: (_: ConstList) => U,
+    f: (_: ConstMap) => U,
+    g: (_: LiteralValue) => U,
+    z: (_: Identifier) => U,
+): U {
+    switch (r.type) {
+        case SyntaxType.ConstList: return e(r as ConstList)
+        case SyntaxType.ConstMap: return f(r as ConstMap)
+        case SyntaxType.DoubleConstant: return g((r as DoubleConstant).value)
+        case SyntaxType.IntConstant: return g((r as IntConstant).value)
+        default: return z(r as Identifier)
+    }
+}
+
+const transformConst = (fld: ConstValue) =>
+    constTransform(
+        fld,
+        (e) => `list<${transformField(e)}>`,
+        (f) => `map<${transformField(f)}>`,
+        getLiteralVal,
+        (z) => `[${z.value}](#${z.value})`,
+    )
+
 /**
  * Type Definitions
  */
@@ -86,8 +157,8 @@ const structFieldRow = (fld: FieldDefinition) => [
     fld.name.value,
     transformField(fld.fieldType),
     fld.comments.length ? fld.comments[0].value : '',
-    fld.requiredness,
-    fld.defaultValue || '',
+    fld.requiredness || '',
+    fld.defaultValue ? transformConst(fld.defaultValue) : '',
 ]
 
 const structDefinitionTable = (def: StructDefinition) => [{
